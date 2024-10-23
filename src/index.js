64a370af017f918d640c123a62f06ecaaceb4da7
dsaulcuevaramos <dsaulcuevaramos@gmail.com>
2024-10-23 18:59:51 -0500

    muchos cambios

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/',(req,res)=>{
+    res.send('!Hola perrillos¡');
+});
+
+app.listen(port, ()=>{
+   console.log(`servidor se corre en tu cara http://localhost:${port}`); 
+});
